e71d9567b4211ba9b5909a89249a260aa1caf2c9
Vinicius Massao Dziewulski <45210162+ViniciusMassao@users.noreply.github.com>
2020-09-16 10:03:49 -0300

    Delete IO.java

--- src/IO.java
+++
@@ -1,8 +0,0 @@
-public class IO {
-    private java.io.PrintStream out = null;
-
-    public IO(java.io.PrintStream o) { out = o;	}
-    public void write(String s) {
-        out.println(s);
-    }
-}
